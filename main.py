ca7fb802ac1541a105dbda29387fc57b7878e868
redblackva <21judah.hurr@utcportsmouth.org>
2022-04-22 16:29:30 +0000

    make thigns worek

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
+import matplotlib.pyplot as plt
+import math
+
+import math
+
+
+def primeFactorsCalc(n):
+  primeList = []
+  # even number divisible
+  while n % 2 == 0:
+    primeList.append(2)
+    n = n / 2
+
+  # n became odd
+  for i in range(3, int(math.sqrt(n))+1, 2):
+
+    while (n % i == 0):
+      primeList.append(int(i))
+      n = n / i
+
+  if n > 2:
+    primeList.append(int(n))
+  
+  return primeList
+
+
+"""
 def primeFactorsCalc(n):
   prime_factors = set()
   if n % 2 == 0:
@@ -13,43 +40,55 @@ def primeFactorsCalc(n):
         n = n // factor
         if n == 1:
           return prime_factors
+"""
+
 
 def persistanceCalc(n):
-  start = n
-  print("Start term  = ", n)
-  persistance = 0
-  while len(str(n)) > 1:
-    next = 1
-    for i in str(n):
-      next *= int(i)
-    n = next
-    persistance += 1
-    print("Term ", persistance + 1, " = ", n)
-  print(str(start) + "\'s persistance is", persistance)
-  print("Prime factors of", start, "are", primeFactorsCalc(start), "\n")
+    start = n
+    print("Start term  = ", n)
+    persistance = 0
+    while len(str(n)) > 1:
+        next = 1
+        for i in str(n):
+            next *= int(i)
+        n = next
+        persistance += 1
+        print("Term ", persistance + 1, " = ", n)
+    print(str(start) + "\'s persistance is", persistance)
+    print("Prime factors of", start, "are", primeFactorsCalc(start), "\n")
+
 
 def quickPC(n):
-  start = n
-  persistance = 0
-  while len(str(n)) > 1:
-    next = 1
-    for i in str(n):
-      next *= int(i)
-    n = next
-    persistance += 1
-  return (persistance, start)
+    start = n
+    persistance = 0
+    while len(str(n)) > 1:
+        next = 1
+        for i in str(n):
+            next *= int(i)
+        n = next
+        persistance += 1
+    return (persistance, start)
+
+print(primeFactorsCalc(277777788888899))
+
 
 xpoints = []
 ypoints = []
 largest = [0, 0]
-for i in range(1000000):
-  Pi = quickPC(i)
-  if Pi[0] > largest[0]:
-    persistanceCalc(i)
-    largest = Pi
-  xpoints.append(Pi)
-  ypoints.append(i)
+for i in range(100, 1000000000000000):
+    singleDigit = True
+    pfs = primeFactorsCalc(i)
+    for j in range(len(pfs)):
+        if pfs[j] > 9:
+            singleDigit = False
+    if singleDigit == True:
+        Pi = quickPC(i)
+        if Pi[0] > largest[0]:
+            persistanceCalc(i)
+            largest = Pi
+        ypoints.append(Pi[0])
+        xpoints.append(Pi[1])
 
 print(len(xpoints), len(ypoints))
 plt.scatter(xpoints, ypoints)
-plt.show()
+plt.savefig('saved_figure.png')
